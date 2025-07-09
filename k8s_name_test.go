a72dbaed1a092865623a6712361326aef805a537
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-07-09 16:43:33 +0200

    add tests

--- k8s_name_test.go
+++ k8s_name_test.go
@@ -5,6 +5,8 @@
 package k8s_test
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
@@ -57,3 +59,46 @@ var _ = DescribeTable("BuildName",
 	Entry("following dash", []string{"my-valid-name---"}, k8s.Name("my-valid-name")),
 	Entry("with number", []string{"my-valid-name-f16"}, k8s.Name("my-valid-name-f16")),
 )
+
+var _ = Describe("Name methods", func() {
+	It("Validate returns error for empty name", func() {
+		var n k8s.Name = ""
+		err := n.Validate(context.Background())
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("Validate returns error for name longer than 253", func() {
+		var n k8s.Name = k8s.Name(make([]byte, 254))
+		err := n.Validate(context.Background())
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("Validate returns nil for valid name", func() {
+		var n k8s.Name = "valid-name"
+		err := n.Validate(context.Background())
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("String returns the string value", func() {
+		var n k8s.Name = "foo-bar"
+		Expect(n.String()).To(Equal("foo-bar"))
+	})
+
+	It("Bytes returns the byte slice", func() {
+		var n k8s.Name = "foo-bar"
+		Expect(n.Bytes()).To(Equal([]byte("foo-bar")))
+	})
+
+	It("Ptr returns a pointer to the Name", func() {
+		var n k8s.Name = "foo-bar"
+		ptr := n.Ptr()
+		Expect(ptr).ToNot(BeNil())
+		Expect(*ptr).To(Equal(n))
+	})
+
+	It("Add returns a new Name with concatenated value", func() {
+		var n k8s.Name = "foo"
+		result := n.Add("bar")
+		Expect(result).To(Equal(k8s.Name("foo-bar")))
+	})
+})
